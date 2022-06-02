a71315db55b21bc880e0c112ef8b674e5048361e
Smeany <sven.oehlen@alumni.fh-aachen.de>
2022-06-02 20:04:21 +0200

    Update solution.cpp
    updated to o(n) space

--- 238. Product of Array Except Self/solution.cpp
+++ 238. Product of Array Except Self/solution.cpp
@@ -2,16 +2,15 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         vector<int> ret(nums.size());
-        vector<int> bck(nums.size());
         ret[0] = 1;
-        bck[nums.size() - 1] = 1;
         for(int i = 1; i < nums.size(); i++){
             ret[i] = ret[i-1] * nums[i - 1];
-            bck[nums.size()-1-i] = bck[nums.size()-i] * nums[nums.size()-i];
         }
         
-        for(int i = 0; i < nums.size() - 1; i++) {
-            ret[i] *= bck[i];
+        int n = 1;
+        for(int i = nums.size() - 2; i >= 0; i--) {
+            n *= nums[i+1];
+            ret[i] *= n;
         }
         
         return ret;
